bd3735a589474eb19c6e86d4a6ffbd3a3b21091a
Carter B. Smith <>
2014-08-14 21:17:47 -0500

    setting up grunt

---
+++ gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    less: {
+      development: {
+        options: {
+          paths: ["dev/components/bootstrap/less"]
+        },
+        files: {
+          "css/bootstrap.css": "dev/less/styles.less"
+        }
+      },
+    },
+    uglify: {
+      bootstrap: {
+        files: {
+          'js/bootstrap.min.js': ['dev/components/bootstrap/js/carousel.js']
+        }
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('css', ['less']);
+  grunt.registerTask('js', ['uglify']);
+
+};
