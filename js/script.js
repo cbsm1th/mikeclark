87bb0780890012e64eacbd0abc3a8b9a76eb3753
Carter B. Smith <>
2014-08-27 23:16:25 -0500

    about section and carousel

--- js/script.js
+++ js/script.js
@@ -2,20 +2,16 @@ $(document).ready(function() {
  
   $("#owl-demo").owlCarousel({
  
-      //navigation : true, // Show next and prev buttons
-      slideSpeed : 300,
-      paginationSpeed : 400,
-      singleItem:true,
-      pagination: false,
-      rewindNav: true,
-      autoPlay: true
- 
-      // "singleItem:true" is a shortcut for:
-      // items : 1, 
-      // itemsDesktop : false,
-      // itemsDesktopSmall : false,
-      // itemsTablet: false,
-      // itemsMobile : false
+      center: true,
+      items: 1,
+      loop: true,
+      responsive:{
+        600:{
+            items:1
+        }
+      },
+      autoplay:true,
+      autoplayTimeout:2000
  
   });
  
